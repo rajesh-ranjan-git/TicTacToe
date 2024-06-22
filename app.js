4f9c9af497335b315de9352653730525c83e4a3e
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-06-22 18:19:20 +0530

    Sound Effects alive

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ let newGame = document.querySelector(".new");
 let reset = document.querySelector(".reset");
 let win = document.querySelector(".win-container");
 let winMsg = document.querySelector(".win p");
+let loading = document.querySelector(".loading");
 let counter = 0;
 let temp = 0;
 let turn = true;
+
+let game_new = document.querySelector("#game_new");
+let game_winner = document.querySelector("#game_winner");
+let game_draw = document.querySelector("#game_draw");
+let game_chance = document.querySelector("#game_chance");
+let game_reset = document.querySelector("#game_reset");
+let click_check = "";
 
 let winPattern = [
   [0, 1, 2],
@@ -18,6 +26,13 @@ let winPattern = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+
+const loader_func = () => {
+  container.style.display = "none";
+  reset.style.display = "none";
+  win.style.display = "none";
+  loading.style.display = "block";
+};
 
 const disableAll = () => {
   container.style.display = "none";
@@ -46,6 +61,7 @@ const draw = () => {
     winMsg.innerText = "Draw !";
     win.style.display = "block";
     disableAll();
+    return "Draw";
   }
 };
 
@@ -56,6 +72,7 @@ boxes.forEach((box) => {
       if (box.innerText == "") {
         if (turn == true) {
           box.innerText = "X";
+          box.style.color = "red";
           turn = false;
           checkWinner(box.innerText);
         } else {
@@ -66,14 +83,29 @@ boxes.forEach((box) => {
       }
     }
 
-    draw();
+    let check_draw = draw();
+    if (click_check === "Won") {
+      game_winner.play();
+    } else if (check_draw === "Draw") {
+      game_draw.play();
+    } else {
+      game_chance.play();
+    }
   });
 });
 
 newGame.addEventListener("click", () => {
-  window.location.reload();
+  game_new.play();
+  loader_func();
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
 });
 
 reset.addEventListener("click", () => {
-  window.location.reload();
+  game_reset.play();
+  loader_func();
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
 });
